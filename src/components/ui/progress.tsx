5063a1fa6827a40ceccd0b437c3b3a81967fcea8
nanoeman <nanoeman@gmail.com>
2026-07-28 00:17:57 +0200

    Fix: progress.tsx tipos de ref

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,27 +1,26 @@
 "use client";
 
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
-const Progress = React.forwardRef<
-  React.HTMLAttributes<HTMLDivElement>,
-  React.ComponentPropsWithoutRef<"div"> & { value?: number }
->(({ className, value = 0, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-taupe-100",
-      className
-    )}
-    {...props}
-  >
+interface ProgressProps {
+  value?: number;
+  className?: string;
+}
+
+function Progress({ className, value = 0 }: ProgressProps) {
+  return (
     <div
-      className="h-full bg-forest-600 transition-all duration-300"
-      style={{ width: `${value}%` }}
-    />
-  </div>
-));
-Progress.displayName = "Progress";
+      className={cn(
+        "relative h-2 w-full overflow-hidden rounded-full bg-taupe-100",
+        className
+      )}
+    >
+      <div
+        className="h-full bg-forest-600 transition-all duration-300"
+        style={{ width: `${value}%` }}
+      />
+    </div>
+  );
+}
 
 export { Progress };
